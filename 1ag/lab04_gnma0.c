d659440a8462a7c28f96e59252bef01102295b70
Teodor 'døru' MICU <mteodor@gmail.com>
2012-03-18 22:10:53 +0200

    adăgat suport pentru argc și argv[1]

--- 1ag/lab04_gnma0.c
+++ 1ag/lab04_gnma0.c
@@ -3,9 +3,11 @@
  * a[i,j] = 0 dacă NU există o muchie între vârfurile i și j
  *
  * Programul se compilează și se apelează astfel:
- * $ ./lab04_main0 < gn3_13.txt
+ * $ ./lab04_gnma0 < gnma3_13.txt
+ *   sau
+ * $ ./lab04_gnma0 gnma3_13.txt
  *
- * "gn3_13.txt" este un fișier ce conține pe prima linie numărul nodurilor și
+ * "gnma3_13.txt" este un fișier ce conține pe prima linie numărul nodurilor și
  * pe celelalte linii matricea de adiacență (0 sau 1 separate prin spațiu)
  * Obs: fișierul trebuie să se termine cu \n (new line)
  */
@@ -19,13 +21,23 @@
 #include "gnma.c"
 
 // operații cu matricea de adiacență a unui graf neorientat
-int main(void)
+int main(int argc, char *argv[])
 {
 	int i, j, v, nr;
+	FILE *fisier;
 	int *mat;
 
-	printf("Introduceți numărul vârfurilor și matricea de adiacență a grafului neorientat:\n");
-	mat = citire_gnma(&nr);
+	if (argc < 2) {
+		printf("Introduceți numărul vârfurilor și matricea de adiacență a grafului neorientat:\n");
+		mat = citire_gnma(&nr);
+	} else {
+		printf("%d %s %s\n", argc, argv[0], argv[1]);
+		fisier = fopen(argv[1], "r");
+		if (NULL == fisier) {
+			fprintf(stderr, "E: fișierul «%s» nu a putut fi deschis\n", argv[1]);
+		}
+		mat = fcitire_gnma(fisier, &nr);
+	}
 	if (NULL == mat) {
 		fprintf(stderr, "E: memorie insuficientă pentru matricea de adiacență\n");
 		return errno;
@@ -39,7 +51,7 @@ int main(void)
 		v = nrvec_gnma(mat, nr, i);
 		if (0 != v) {
 			printf("Nodul «%d» are %d vecini:", i, v);
-			afvec_gnma(mat,nr,i);
+			afvec_gnma(mat, nr, i);
 		}
 	}
 	putchar('\n');
@@ -47,7 +59,7 @@ int main(void)
 	printf("Graful neorientat are următoarele muchii:");
 	for (i = 1; i <= nr; i++) {
 		for (j = 1; j <= nr; j++) {
-			if (muchie_gnma(mat,nr,i,j))
+			if (muchie_gnma(mat, nr, i, j))
 				printf(" [%d;%d]", i, j);
 		}
 	}
