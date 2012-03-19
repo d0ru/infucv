29c2d373977dcf130057d44b694fb43d5b870c10
Teodor 'døru' MICU <mteodor@gmail.com>
2012-03-19 08:18:00 +0200

    program de test operații cu librăria GNLA

---
+++ 1ag/lab04_gnla0.c
@@ -0,0 +1,67 @@
+/*
+ * Operații pe lista de adiacență a unui graf neorientat.
+ *
+ * Programul se compilează și se apelează astfel:
+ * $ ./lab04_gnla0 < gnla3_13.txt
+ *   sau
+ * $ ./lab04_gnla0 gnla3_13.txt
+ *
+ * "gnla3_13.txt" este un fișier ce conține pe prima linie numărul nodurilor și
+ * pe celelalte linii lista de adiacență a grafului neorientat
+ * Obs: fișierul trebuie să se termine cu \n (new line)
+ */
+
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "gnla.h"
+#include "gnla.c"
+
+// operații cu lista de adiacență a unui graf neorientat
+int main(int argc, char *argv[])
+{
+	int i, j, v, nr;
+	FILE *fisier;
+	struct nod *liad;
+
+	if (argc < 2) {
+		printf("Introduceți numărul vârfurilor și lista de adiacență a grafului neorientat:\n");
+		liad = citire_gnla(&nr);
+	} else {
+		printf("%d %s %s\n", argc, argv[0], argv[1]);
+		fisier = fopen(argv[1], "r");
+		if (NULL == fisier) {
+			fprintf(stderr, "E: fișierul «%s» nu a putut fi deschis\n", argv[1]);
+		}
+		liad = fcitire_gnla(fisier, &nr);
+	}
+	if (NULL == liad) {
+		fprintf(stderr, "E: memorie insuficientă pentru lista de adiacență\n");
+		return errno;
+	}
+
+	printf("Lista de adiacență a grafului neorientat:\n");
+	afisare_gnla(liad, nr);
+	putchar('\n');
+
+	for (i = 1; i <= nr; i++) {
+		v = nrvec_gnla(liad, nr, i);
+		if (0 != v) {
+			printf("Nodul «%d» are %d vecini:", i, v);
+			afvec_gnla(liad, nr, i);
+		}
+	}
+	putchar('\n');
+
+	printf("Graful neorientat are următoarele muchii:");
+	for (i = 1; i <= nr; i++) {
+		for (j = 1; j <= nr; j++) {
+			if (muchie_gnla(liad, nr, i, j))
+				printf(" [%d;%d]", i, j);
+		}
+	}
+	putchar('\n');
+	return 0;
+}
