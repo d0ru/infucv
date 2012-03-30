f2471ad85c7097975b9f9cb0e4d2c81455bf87bc
Teodor 'døru' MICU <mteodor@gmail.com>
2012-03-30 22:38:06 +0300

    FII'12 runda nr. 1 -- inversiuni
    Calculul numărului de inversiuni are complexitatea timp O(n²) și
    constă în compararea fiecărui număr întreg din vector cu toate
    numerele din vector ce se găsesc înaintea lui.
    Numărul maxim de inversiuni se obține când toate numerele sunt
    sortate în ordine strict descrescătoare.

---
+++ fii12/runda1/inversiuni.c
@@ -0,0 +1,112 @@
+/**
+ * Fie x1, x2,.. xn o secvență de «n» numere întregi distincte.
+ * Perechea (i,j) se numește inversiune dacă xi > xj, cu 1 ≤ i < j ≤ n.
+ * Exemplu: secvența "2 4 3 1 5 6 9" are 4 inversiuni.
+ *
+ * Cerință
+ * -------
+ * Scrieți un program care pentru fiecare secvență de numere citită
+ * din fișierul de intrare, scrie în fișierul de ieșire numărul de
+ * inversiuni din secvență.
+ * Fiecare rezultat va fi scris pe o linie separată.
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// citire secvență de numere
+int *citire_secventa(FILE *fisier, unsigned short int *dimensiune);
+
+// calculează numărul de inversiuni dintr-o secvență de numere
+int nr_inversiuni(unsigned short int dimensiune, int secventa[]);
+
+/**
+ * Soluție
+ * =======
+ * Se citește pe rând câte o secvență de numere întregi din fișierul
+ * de intrare «inversiuni.in». Dacă fișierul este gol sau au fost
+ * citite toate secvențele de numere, funcția «citire_secventa» va
+ * returna NULL.
+ * Memoria se alocă dinamic pentru fiecare secvență în parte și este
+ * eliberată înainte de citirea următoarei secvențe de numere.
+ * Am folosit "int" pentru numere întregi generice în limbajul C.
+ *
+ * Calculul numărului de inversiuni are complexitatea timp O(n²) și
+ * constă în compararea fiecărui număr întreg din vector cu toate
+ * numerele din vector ce se găsesc înaintea lui.
+ * Numărul maxim de inversiuni se obține când toate numerele sunt
+ * sortate în ordine strict descrescătoare.
+ *
+ * Limitare
+ * ========
+ * Programul nu tratează situația în care la intrare sunt date (eronat)
+ * mai multe sau mai puține numere decât a fost declarat prin numărul
+ * ce precede secvența de numere.
+ * Date din fișierul de intrare se consideră a fi corecte.
+ */
+int main(void)
+{
+	FILE *fintrare;				// "r" din «inversiuni.in»
+	FILE *fiesire;				// "w" în «inversiuni.out»
+	int *secventa;				// vector alocat dinamic
+	unsigned short int dimensiune;		// dimensiune secvență
+	int nrinv;				// inversiuni în secvență
+
+	fintrare = fopen("inversiuni.in", "r");
+	if (NULL == fintrare) {
+		fprintf(stderr, "E: fișierul «inversiuni.in» nu a putut fi deschis pentru citire\n");
+		return errno;
+	}
+
+	fiesire = fopen("inversiuni.out", "w");
+	if (NULL == fiesire) {
+		fprintf(stderr, "E: fișierul «inversiuni.out» nu a putut fi deschis pentru scriere\n");
+		return errno;
+	}
+
+	// dacă fișierul de intrare este gol, bucla nu se execută
+	while (NULL != (secventa = citire_secventa(fintrare, &dimensiune))) {
+		nrinv = nr_inversiuni(dimensiune, secventa);
+		fprintf(fiesire, "%i\n", nrinv);
+		free(secventa);			// eliberează memoria alocată
+	}
+	return 0;
+}
+
+// COMPLEXITATE timp Θ(n) | spațiu Θ(n)
+int *citire_secventa(FILE *fisier, unsigned short int *dimensiune)
+{
+	int *secventa, *numar;
+	short int i;				// maxim 10,000
+
+	// citesc dimensiunea secvenței de numere întregi
+	if (1 != fscanf(fisier, "%hu", dimensiune))
+		return NULL;			// eroare de citire sau
+						// sfârșit de fișier
+
+	secventa = (int *) malloc(sizeof(int) * (*dimensiune));
+	if (NULL == secventa) {
+		fprintf(stderr, "E: nu s-a putut aloca memorie pentru o secvență de «%i» numere întregi\n", *dimensiune);
+		return NULL;
+	}
+
+	// nu se verifică corectitudinea sau numărul datelor de intrare
+	for (i = 0, numar = secventa; i < *dimensiune; i++, numar++)
+		fscanf(fisier, "%i", numar);
+	return secventa;
+}
+
+// COMPLEXITATE timp Θ(n²) | spațiu Θ(n)
+int nr_inversiuni(unsigned short int dimensiune, int secventa[])
+{
+	int inversiuni;				// maxim 50,000,000
+	short int i, j;				// maxim 10,000
+
+	inversiuni = 0;
+	for (j = 1; j < dimensiune; j++)
+		for (i = 0; i < j; i++)
+			if (secventa[i] > secventa[j])
+				inversiuni++;
+	return inversiuni;
+}
