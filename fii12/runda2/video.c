218b9a63e443b8a9d8d13b2968539170a1cc8fdc
Teodor 'døru' MICU <mteodor@gmail.com>
2012-04-09 21:54:29 +0300

    np: utilizează «long long int» pentru 64-biți
    Pe un sistem i386 «long int» are doar 32-biți, deci este insuficient la
    calculul numărului minim de pauze ce poate ajunge până la 2³⁴.
    
      np < t · (b / d) / p = t2 / p ≤ t2 ≤ 14400 · 2²⁰ < 2³⁴

--- fii12/runda2/video.c
+++ fii12/runda2/video.c
@@ -12,13 +12,6 @@
  * În scopuri didactice, se consideră că timpul și cantitățile de
  * informație sunt mărimi continue sau, prin alte cuvinte, sunt admise
  * și părți fracționare de secundă și octet.
- *
- * Restricții și precizări
- * -----------------------
- *  1024 ≤ d ≤ 1073741823
- *  d < b ≤ 1073741824
- *  1 ≤ p ≤ 3600
- *  1 ≤ t ≤ 14400
  */
 
 //#define NDEBUG
@@ -30,11 +23,17 @@
 /**
  * Soluție
  * -------
+ * b - viteza de transmisie a informației pentru redare fără întreruperi
+ * d - viteza reală de transmisie a informației (d < b)
+ * t - timpul de redare a filmului fără pauze
+ * p - durata total admisibilă a fiecărie pauze în parte
+ *
  * t2 - timpul de redare a filmului cu pauze
  *   t2 = t · b / d
  *
  * np - numărul minim de pauze
  *   np = ⌈(t2 - t) / p⌉ = ⌈t · (b/d - 1) / p⌉ = ⌈t · (b-d) / (d · p)⌉
+ *   np < t · (b / d) / p = t2 / p ≤ t2
  *
  * ti - timpul per interval (pauză+redare)
  *   ti = t2  / np = t · (b/d) / np
@@ -45,21 +44,22 @@
  * tp - timpul de pauza a filmului pe fiecare din cel «np» intervale
  *   tp = ti - tr = ti · (b-d) / b = t · (b-d) / (d · np)
  *
+ * s - capacitate memorie tampon
  * ⇒  s = tp · d = t · (b-d) / np
+ *    s < t2 · b ≤ 14400 · 2⁵⁰ < 2⁶⁴
  */
 int main(void)
 {
 	FILE *fintrare;				// "r" din «video.in»
 	FILE *fiesire;				// "w" în «video.out»
-	double b;			// viteza transmisie a informației necesară pentru redare fără întreruperi
-	double d;			// viteza reală (d < b)
-	double t;			// timpul de redare a filmului fără pauze
-	float p;			// durata total admisibilă a pauzelor
-	double t2;
-	int np;				// numărul minim de pauze
-	long double s;			// capacitate memorie tampon
+	double b, d;				// 2¹⁰ ≤ d < b ≤ 2³⁰
+	short int t;				// 1 ≤ t ≤ 14400
+	short int p;				// 1 ≤ p ≤ 3600
+	double t2;				// t < t2 ≤ 14400 · 2²⁰ (> 2³³)
+	long long int np;			// np < t2 < 2³⁴
+	double s;				// s < t2 · b < 2⁶⁴
 #ifndef NDEBUG
-	double ti, tp, tr;
+	double ti, tp, tr;			// t* < t2 < 2³⁴
 #endif
 
 	fintrare = fopen("video.in", "r");
@@ -75,7 +75,7 @@ int main(void)
 	}
 
 	// citesc datele de intrare
-	if (4 != fscanf(fintrare, "%lf %lf %lf %f", &d, &b, &t, &p))
+	if (4 != fscanf(fintrare, "%lf %lf %hi %hi", &d, &b, &t, &p))
 		return errno;			// eroare de citire
 
 	//depanare: verific datele de intrare
@@ -88,23 +88,26 @@ int main(void)
 	assert(1 <= t);
 	assert(t <= 14400);
 
-	t2 = b * t / d;				// timpul de redare a filmului cu pauze
-	np = (int)(t * (b-d) / (d * p));	// np = ⌈t · (b-d) / (d · p)⌉
+	t2 = t * b / d;
+	np = (long long int)(t * (b - d) / (d * p));
 	if ((np * p) < (t2 - t))
 		++np;
 	//depanare: numărul de pauze trebuie să fie cel puțin 1
 	assert(np > 0);
 
 #ifndef NDEBUG
-	fprintf(stdout, "I: t=%lg, t2=%lg\n", t, t2);
+	fprintf(stdout, "I: b-d=%lg t=%hi, t2=%lg\n", (b-d), t, t2);
 	tp = t2 - t;				// timpul total pauze
-	fprintf(stdout, "I: tp=%lg, p=%g, np=%i\n", tp, p, np);
+	fprintf(stdout, "I: tp=%lg, p=%hi, np=%lli\n", tp, p, np);
 	ti = t2 / np;				// timpul per interval (pauză+redare)
 	tr = t / np;				// timpul de redare a filmului
 	tp = ti - tr;
 	fprintf(stdout, "I: ti=%lg, tp=%lg tr=%lg\n", ti, tp, tr);
 #endif
 	s = t * (b-d) / np;			// s = tp · d = t · (b-d) / np
-	fprintf(fiesire, "%Lg\n", s);
+	if (s == (long long int)s)
+		fprintf(fiesire, "%.0lf\n", s);
+	else
+		fprintf(fiesire, "%lf\n", s);
 	return 0;
 }
