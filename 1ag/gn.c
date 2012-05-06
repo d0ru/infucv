d7e3a975303d941b169d65d861f1a02fb276de1d
Teodor 'døru' MICU <mteodor@gmail.com>
2012-05-06 17:52:02 +0300

    conversii de reprezentare a grafurilor neorientate
    Am implementat funcții de conversie de la reprezentarea
    grafurilor prin listă de adiacență la reprezentarea prin
    matrice de adiacență sau matrice de muchii.

--- 1ag/gn.c
+++ 1ag/gn.c
@@ -3,12 +3,16 @@
  * Funcții utilizate des în operațiile cu grafuri.
  */
 
+//#define NDEBUG
+
+#include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "gn.h"
 
-// COMPLEXITATE Θ(n)
+// COMPLEXITATE θ(n)
 bool *mkviz(int nrvarf)
 {
 	int i;
@@ -25,4 +29,63 @@ bool *mkviz(int nrvarf)
 	for (i = 0; i < nrvarf; i++)
 		viz[i] = false;
 	return viz;
+}
+
+// timp θ(n+m) | spațiu θ(n+m)
+int *gnla2ma(const struct nod graf[], int nrvarf)
+{
+	int i, j;
+	int *mat;
+	struct nod *elem;
+
+	mat = (int *) calloc(nrvarf * nrvarf, sizeof(int));
+	if (NULL == mat)
+		return NULL;
+
+	// parcurg lista de vecini
+	for (i = 0; i < nrvarf; i++) {
+		// verificare sanitară
+		assert(1 <= graf[i].nr && graf[i].nr <= nrvarf);
+
+		j = (graf[i].nr - 1) * nrvarf;	// început linie
+		elem = graf[i].urm;
+		while (NULL != elem) {
+			mat[j + elem->nr - 1] = 1;
+			elem = elem->urm;
+		}
+	}
+	return mat;
+}
+
+// timp θ(n+m) | spațiu θ(n+m)
+int *gnla2mm(const struct nod graf[], int nrvarf, int *nrmuchii)
+{
+	int i, j;
+	int *mat;
+	struct nod *elem;
+
+	*nrmuchii = nrmuc_gnla(graf, nrvarf);
+	printf("nrmuchii = %d\n", *nrmuchii);
+	mat = (int *) calloc(*nrmuchii * 2, sizeof(int));
+	if (NULL == mat)
+		return NULL;
+
+	// parcurg lista de vecini
+	for (i = 0, j = 0; i < nrvarf; i++) {
+		// verificare sanitară
+		assert(1 <= graf[i].nr && graf[i].nr <= nrvarf);
+
+		elem = graf[i].urm;
+		while (NULL != elem) {
+			if (graf[i].nr < elem->nr) {
+				// verificare sanitară
+				assert(j < *nrmuchii);
+				mat[j] = graf[i].nr;
+				mat[j + *nrmuchii] = elem->nr;
+				++j;
+			}
+			elem = elem->urm;
+		}
+	}
+	return mat;
 }
--- 1ag/gn.h
+++ 1ag/gn.h
@@ -3,10 +3,26 @@
  * Funcții utilizate des în operațiile cu grafuri.
  */
 
+#include "gnla.h"
+#include "gnma.h"
+#include "gnmm.h"
+
 #ifndef INFUCV_GN
 #define INFUCV_GN
 // GN - graf neorientat
 
 // alocă un vector de vizitare și inițializează cu "false"
 bool *mkviz(int nrvarf);
+
+// conversii de reprezentare graf neorientat: LA ↦ MA, MM
+int *gnla2ma(const struct nod graf[], int nrvarf);
+int *gnla2mm(const struct nod graf[], int nrvarf, int *nrmuchii);
+
+// conversii de reprezentare graf neorientat: MA ↦ LA, MM
+struct nod *gnma2la(const int *graf, int nrvarf);
+int *gnma2mm(const int *graf, int nrvarf, int *nrmuchii);
+
+// conversii de reprezentare graf neorientat: MM ↦ LA, MA
+struct nod *gnmm2la(const int *graf, int nrmuchii);
+int *gnmm2ma(const int *graf, int nrmuchii);
 #endif
