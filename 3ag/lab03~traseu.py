e59f970d6a39be9d52023111e832e2faed05076e
Teodor 'døru' MICU <mteodor@gmail.com>
2013-04-07 16:01:28 +0300

    program test cod/decod traseu ordinal
    Prin condiția de generare aleatoare, nu ar trebui întâlnit vreun
    traseu codificat prin ordine (poziție) care să fie invalid.

--- 3ag/lab03~traseu.py
+++ 3ag/lab03~traseu.py
@@ -2,17 +2,23 @@
 # -*- coding: utf-8 -*-
 """
 Acesta este un program test codificare și decodificare
-a unui cromozom reprezentat prin listă adiacențe.
+a unui cromozom reprezentat prin listă adiacențe și
+a unui cromozom reprezentat prin listă de ordin.
 """
 
 import crmz
 
 
 def main():
-    cr = crmz.CrmzCA(9)
-    #cr.reset(1, 2, 4, 3, 8, 5, 9, 6, 7)
-    print('cr#%d: %s (%d)' % (cr.id, cr.sir(), len(cr.g)))
-    print('    = %s' % cr.traseu())
+    cra = crmz.CrmzCA(9)
+    #cra.reset(1, 2, 4, 3, 8, 5, 9, 6, 7)
+    print('cr#%d: %s (%d)' % (cra.id, cra.sirint(True, ', '), len(cra.g)))
+    print('    = %s' % cra.traseu())
+    print('')
+    cro = crmz.CrmzCO(9)
+    #cro.reset(1, 2, 4, 3, 8, 5, 9, 6, 7)
+    print('cr#%d: %s (%d)' % (cro.id, cro.sirint(True, ', '), len(cro.g)))
+    print('    = %s' % cro.traseu())
 
 
 if __name__ == '__main__':
