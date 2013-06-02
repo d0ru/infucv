8b3dc09a00b57994bf1a65152d741fd3a7c360a1
Teodor 'døru' MICU <mteodor@gmail.com>
2013-06-02 14:53:18 +0300

    mbin(): mutația binară tare

--- 3ag/pcrmz.py
+++ 3ag/pcrmz.py
@@ -88,8 +88,8 @@ class PCrmz(object):
             self.cr[ic2].g[s:f] = tmp[:]                # cr2 ← tmp
             del tmp
         if info:
-            print("%2d: %s" % (self.cr[ic1].id, self.cr[ic1].sir()))
-            print("%2d: %s" % (self.cr[ic2].id, self.cr[ic2].sir()))
+            print('%2d: %s' % (self.cr[ic1].id, self.cr[ic1].sir()))
+            print('%2d: %s' % (self.cr[ic2].id, self.cr[ic2].sir()))
 
     def xbin(self, fadecv, nc=0, nt=0, info=False, selinfo=False):
         """ Încrucișarea binară a unui set aleator de cromozomi
@@ -114,3 +114,43 @@ class PCrmz(object):
             ic2 = crsel[ri]             # secund
             del crsel[ri]
             self.x2bin(ic1, ic2, nt, info=info)     # încrucișare
+
+    def m1bin(self, ic, pm = 0.25, info=False):
+        """ Mutația binară tare a unui singur cromozom
+        - ic: index cromozom în vectorul cr[]
+        - pm: probabilitatea de mutație ∈ [0, 1] (implicit 0.25)
+
+        Metoda operează direct pe cromozom și înlocuiește
+        genele ce au o probabilitate aleatoare ρ < pm.
+        """
+        print('M2 gene selectate:', end='')
+        for i in range(len(self.cr[ic].g)):
+            rho = random.random()       # [0, 1)
+            if rho < pm:
+                print(' %d' % i, end='')
+                if self.cr[ic].g[i]:    # =1
+                    self.cr[ic].g[i] = 0
+                else:
+                    self.cr[ic].g[i] = 1
+        print('')
+        print('%2d: %s' % (self.cr[ic].id, self.cr[ic].sir()))
+
+    def mbin(self, fadecv, nc = 0, pm = 0.25, info=False, selinfo=False):
+        """ Mutația binară tare a unui set aleator de cromozomi
+        - fadecv(cr): funcția de adecvare (sau fitnes)
+        - fsel(fadecv, nc): funcția de selecție cromozomi
+        - nc: numărul de cromozomi selectați (implicit 20% din populație)
+        - pm: probabilitatea de mutație ∈ [0, 1] (implicit 0.25)
+
+        Metoda operează direct pe populație și înlocuiește
+        genele ce au o probabilitate aleatoare ρ < pm.
+        """
+        if nc <= 0:
+            nc = len(self.cr) // 5  # valoarea implicită
+
+        crsel = self.sel(fadecv, nc, info=selinfo)  # selecție
+        if info:
+            print('M cromozomi selectați: %s' % crsel)
+
+        for ic in crsel:
+            self.m1bin(ic, pm, info=info)           # mutație cromozom
