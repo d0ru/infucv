30f226528e5b958e63544b56b2ea2094cb63672a
Teodor 'døru' MICU <mteodor@gmail.com>
2013-06-02 12:34:02 +0300

    pcrmz.sel(): test peste o funcție cu o singură variabilă
    f(x) = -sin(x) / x

---
+++ 3ag/lab06~sfx1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Acesta este un program test de selectare aleatoare a unei
+populații de cromozomi folosind metoda ruletei.
+"""
+
+import crmz
+import pcrmz
+from numpy import sin
+
+
+def fx1(x):
+    return -sin(x) / x
+
+a1, b1 = -4, 4
+xmin = 9.76996262e-15
+fx1min = fx1(xmin)
+
+def adecv(cr):
+    """ Funcția de adecvare asociată funcției fx1 """
+    x1 = cr.getv(1)
+    return 1. / abs(fx1min - fx1(x1))
+
+def main():
+    m = 10      # dimensiune populație
+    popu = pcrmz.PCrmz()
+    # inițializare populație
+    for i in range(m):
+        popu.cr.append(crmz.CrmzCR(pr = 4))
+        popu.cr[-1].reset( (a1, b1, '') )
+
+    # afișare populație
+    print('Cromozomi generați automat:')
+    for c in popu.cr:
+        x1 = c.getv(1)
+        print('%2d: %s, coeficient de adecvare %f, f(%.3f)=%f' % (c.id, c.sir(), adecv(c), x1, fx1(x1)))
+
+    print('')
+    print popu.sel(adecv, info=True)
+
+
+if __name__ == '__main__':
+    main()
