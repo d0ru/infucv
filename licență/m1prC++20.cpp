12a4d52b15e78a6e783c2c5b5d185f514f5d6961
Teodor 'døru' MICU <mteodor@gmail.com>
2013-07-14 19:21:55 +0300

    grilă M1, pr.C++ #20

---
+++ licență/m1prC++20.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+class A {
+private:
+	int x, y;
+public:
+	A() { x=0; y=0; }
+	A(int xi, int yi) {
+		x = xi; y = yi;
+	}
+	void afis() {
+		cout << x << " " << y << " ";
+	}
+};
+
+
+int main(void)
+{
+	A A1,A2(10,20);
+	A1.afis();
+	A2.afis();
+}
