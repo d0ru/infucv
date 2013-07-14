852b9034d8dd195a92ad37eda6de33d821a5d686
Teodor 'døru' MICU <mteodor@gmail.com>
2013-07-14 13:27:29 +0300

    grilă M1, pr.C #11

---
+++ licență/m1prC11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float x = 10;
+	int y = 7;
+	float t[] = { 3, 4, 1 };
+	float *q = &x;
+	int *r = &y;
+	void *p;
+
+	q = t;		// A
+	p = t;		// B
+	p = &y;		// C
+	p = r;		// D
+	x = &t[2];	// E
+	return 0;
+}
