2a30a728fbc374b7309b415a4a594e90a838bcee
Teodor 'døru' MICU <mteodor@gmail.com>
2013-07-14 19:02:43 +0300

    grilă M1, pr.C++ #14

---
+++ licență/m1prC++14.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+class B {
+	int x;
+public:
+	B(int x=0){ this->x=x; }
+	virtual void f() { x=x+1; }
+	void print() { cout<<x<<" "; }
+};
+
+class D1: public B {
+public:
+	void f() { x=x+5; }
+};
+
+class D2: public B {
+public:
+	void f() { x=x+2; }
+};
+
+
+int main(void)
+{
+	int i;
+	B *b[] = { new B(2), new D1(), new D2(), new B };
+
+	for (i=3; i > 0; i--) { (*b[i]).f(); }
+	for (i=0; i <= 3; i++) b[i]->print();
+}
